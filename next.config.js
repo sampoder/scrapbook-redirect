84f2548748c97865049f5bf83c3f438f731e20ad
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-12 19:35:16 +0000

    Trailing spaces be gone!

--- next.config.js
+++ next.config.js
@@ -11,9 +11,13 @@ module.exports = {
         return [
             { "source": "/attachments/:id/:id2", "destination": "https://scrapbook.hackclub.com/attachments/:id/:id2", permanent: true},
             { "source": "/:id/", "destination": "https://scrapbook.hackclub.com/:id", permanent: true},
+            { "source": "/:id", "destination": "https://scrapbook.hackclub.com/:id", permanent: true},
             { "source": "/:id/:id2/", "destination": "https://scrapbook.hackclub.com/:id/:id2", permanent: true},
+            { "source": "/:id/:id2", "destination": "https://scrapbook.hackclub.com/:id/:id2", permanent: true},
             { "source": "/:id/:id2/:id3/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3", permanent: true},
+            { "source": "/:id/:id2/:id3", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3", permanent: true},
             { "source": "/:id/:id2/:id3/:id4/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3/:id4", permanent: true},
+            { "source": "/:id/:id2/:id3/:id4", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3/:id4", permanent: true},
             { "source": "/_next/static/:id/:id2.css", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.css", permanent: true},
             { "source": "/_next/static/:id/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.js", permanent: true},
             { "source": "/_next/static/chunks/pages/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/chunks/pages/:id2.js", permanent: true }
