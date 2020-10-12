1ce56936a68d976f5945fb619f1a7e91405233a3
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-12 19:25:12 +0000

    Add , permanent: true again

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ module.exports = {
             { "source": "/:id/:id2/:id3/:id4/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3/:id4", permanent: true},
             { "source": "/_next/static/:id/:id2.css", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.css", permanent: true},
             { "source": "/_next/static/:id/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.js", permanent: true},
-            { "source": "/_next/static/chunks/pages/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/chunks/pages/:id2.js" }
+            { "source": "/_next/static/chunks/pages/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/chunks/pages/:id2.js", permanent: true }
         ]
       },
   }
