9ba4b910045b6e2d14037ceffb04d6b3d99b5d31
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-12 19:22:53 +0000

    Add permanent: true

--- next.config.js
+++ next.config.js
@@ -9,13 +9,13 @@ module.exports = {
     },
     async redirects() {
         return [
-            { "source": "/attachments/:id/:id2/", "destination": "https://scrapbook.hackclub.com/attachments/:id/:id2/" },
-            { "source": "/:id/", "destination": "https://scrapbook.hackclub.com/:id" },
-            { "source": "/:id/:id2/", "destination": "https://scrapbook.hackclub.com/:id/:id2" },
-            { "source": "/:id/:id2/:id3/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3" },
-            { "source": "/:id/:id2/:id3/:id4/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3/:id4" },
-            { "source": "/_next/static/:id/:id2.css", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.css" },
-            { "source": "/_next/static/:id/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.js" },
+            { "source": "/attachments/:id/:id2/", "destination": "https://scrapbook.hackclub.com/attachments/:id/:id2/", permanent: true},
+            { "source": "/:id/", "destination": "https://scrapbook.hackclub.com/:id", permanent: true},
+            { "source": "/:id/:id2/", "destination": "https://scrapbook.hackclub.com/:id/:id2", permanent: true},
+            { "source": "/:id/:id2/:id3/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3", permanent: true},
+            { "source": "/:id/:id2/:id3/:id4/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3/:id4", permanent: true},
+            { "source": "/_next/static/:id/:id2.css", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.css", permanent: true},
+            { "source": "/_next/static/:id/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/:id/:id2.js", permanent: true},
             { "source": "/_next/static/chunks/pages/:id2.js", "destination": "https://scrapbook.hackclub.com/_next/static/chunks/pages/:id2.js" }
         ]
       },
