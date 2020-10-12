4a646de168120954bde5aed6394c7c3ab72a6b0f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-12 19:42:56 +0000

    Use env vars!

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
       return [
         {
           source: '/',
-          destination: 'https://scrapbook.hackclub.com/sampoder/',
+          destination: `https://scrapbook.hackclub.com/${process.env.username}/`,
         }
       ]
     },
