79ed239e0c11f1d43b3faa7fc7403c7f70e1ea02
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-12 18:33:30 +0000

    Test?

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-    async rewrites() {
-      return [
+export async function rewrites() {
+    return [
         {
-          source: '/',
-          destination: 'https://scrapbook.hackclub.com/sampoder/',
+            source: '/',
+            destination: 'https://scrapbook.hackclub.com/sampoder/',
         },
-      ]
-    },
-  }
+    ]
+}
