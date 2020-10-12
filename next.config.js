f38ca64ab4c6d80f3aa512111b4a3412e4966910
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-12 20:15:46 +0000

    Add redirect

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ module.exports = {
             { "source": "/attachments/:id/:id2", "destination": "https://scrapbook.hackclub.com/attachments/:id/:id2", permanent: true},
             { "source": "/:id/", "destination": "https://scrapbook.hackclub.com/:id", permanent: true},
             { "source": "/:id", "destination": "https://scrapbook.hackclub.com/:id", permanent: true},
+            { "source": "/api/vercel-finished", "destination": "/api/vercel-finished", permanent: true},
             { "source": "/:id/:id2/", "destination": "https://scrapbook.hackclub.com/:id/:id2", permanent: true},
             { "source": "/:id/:id2", "destination": "https://scrapbook.hackclub.com/:id/:id2", permanent: true},
             { "source": "/:id/:id2/:id3/", "destination": "https://scrapbook.hackclub.com/:id/:id2/:id3", permanent: true},
