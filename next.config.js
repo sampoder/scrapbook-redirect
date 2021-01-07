a3c84338b136641169a00afa18c95e3d28d14240
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-01-07 17:56:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    images: {
+      imageSizes: [18, 36, 54, 24, 48, 72, 96, 144],
+      domains: [
+        'dl.airtable.com',
+        'emoji.slack-edge.com',
+        'cloud-lp0r5yk68.vercel.app'
+      ]
+    },
     async rewrites() {
       return [
         {
